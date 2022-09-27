730715e4b153dab479cf0d5722ec950e8ff4c643
John Aughey <jha@aughey.com>
2022-09-27 01:45:51 +0000

    renaming ci to cd

--- run-ci.sh
+++
@@ -1,9 +0,0 @@
-BUILDLOG=/tmp/build$$.log
-
-echo "=== GIT SHOW ===" > $BUILDLOG
-git show --oneline -s >> $BUILDLOG
-
-echo "=== BUILD ===" >> $BUILDLOG
-sh run-build.sh tee -a $BUILDLOG
-sh copy_artifacts.sh
-cp $BUILDLOG artifacts/build.log
